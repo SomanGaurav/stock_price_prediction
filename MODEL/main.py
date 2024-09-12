0cda41489092d177bfb9fe111f3205aaf0c0317e
Gaurav <gauravsoman19805@gmail.com>
2024-09-12 19:06:09 +0530

    model_interfaced

--- MODEL/main.py
+++ MODEL/main.py
@@ -1,3 +1,22 @@
+from flask import Flask , request 
+from models import Linear_Regression 
+
+app = Flask(__name__) 
 
 
+@app.route("/test")
+def home(): 
+    return "Hello to my server" 
 
+
+@app.route("/forecast" , methods = ["POST"])
+def forecast_out(): 
+   
+    name = request.args.get('stockTick')
+    linear_model = Linear_Regression(name)
+    forecast_output = linear_model.make_prdictions()
+    output = {"forecast" : forecast_output[0] , "modelScore" : forecast_output[1]}
+    return output 
+
+if __name__ == "__main__" : 
+    app.run(debug=True) 
--- MODEL/models.py
+++ MODEL/models.py
@@ -2,29 +2,28 @@ import data_collector
 from sklearn.linear_model import LinearRegression 
 
 class Linear_Regression : 
-    def __init__(self , stock_tick , start) -> None:
+    def __init__(self , stock_tick ) -> None:
         self.stock_tick = stock_tick 
         self.dc = data_collector.DATACOLLECTOR()
         self.lr = LinearRegression()
-        self.start = start 
+        self.start = "2012-01-01" 
 
     def model_train(self): 
         X_train , X_test , y_train , y_test = self.dc.linear_reg_data(self.stock_tick , self.start)
         self.lr.fit(X_train , y_train)
         self.lr_score = self.lr.score(X_test , y_test) 
-        print(self.lr_score)
+        return self.lr_score
 
 
     def make_prdictions(self): 
+        self.model_score = self.model_train()
         inputs = self.dc.pred_data()
         lr_pred = self.lr.predict(inputs) 
-        print(lr_pred)
-
+        return (lr_pred.tolist() , self.model_score) 
+ 
 
 
 
 if __name__ == "__main__" : 
-    linearmodel = Linear_Regression("ADANIENT.NS" , start = "2012-01-01") 
-    linearmodel.model_train()
-    linearmodel.make_prdictions()
-
+    linearmodel = Linear_Regression("ADANIENT.NS") 
+    print(linearmodel.make_prdictions())
